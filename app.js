0e9e5a376d26b08900b775be15a00bd7e5cf8518
Zuke27 <wstaylor27@gmail.com>
2022-02-24 22:47:38 -0500

    Updated app.js logic

---
+++ app.js
@@ -0,0 +1,5 @@
+// select items
+const img = document.getElementById("person-img");
+const author = document.getElementById("");
+const job = document.getElementById("");
+const info = document.getElementById("");
