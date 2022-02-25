95629dcced81bb4873dabd601cb974e96197cf81
Zuke27 <wstaylor27@gmail.com>
2022-02-24 23:05:14 -0500

    Updated nextBtn in html

--- app.js
+++ app.js
@@ -24,9 +24,22 @@ let currentItem = 0;
 
 // load initial item
 window.addEventListener("DOMContentLoaded", function () {
-  const item = reviews[currentItem];
+  showPerson(currentItem);
+});
+
+// show person based on item
+
+function showPerson(person) {
+  const item = reviews[person];
   img.src = item.img;
   author.textContent = item.name;
   job.textContent = item.job;
   info.textContent = item.text;
+}
+
+// show next person
+
+nextBtn.addEventListener("click", function () {
+  currentItem++;
+  showPerson(currentItem);
 });
