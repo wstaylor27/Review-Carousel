c4a90432da884b8bb12ee39435ca80b53c3df25a
Zuke27 <wstaylor27@gmail.com>
2022-02-24 23:02:53 -0500

    Added showPerson logic

--- app.js
+++ app.js
@@ -1,5 +1,32 @@
+// local reviews data
+const reviews = [
+  {
+    id: 1,
+    name: "susan smith",
+    job: "web developer",
+    img: "random http",
+    text: "What's up everybody",
+  },
+];
+
 // select items
 const img = document.getElementById("person-img");
-const author = document.getElementById("");
-const job = document.getElementById("");
-const info = document.getElementById("");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
+
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randomBtn = document.querySelector(".random-btn");
+
+// set starting item
+let currentItem = 0;
+
+// load initial item
+window.addEventListener("DOMContentLoaded", function () {
+  const item = reviews[currentItem];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+});
